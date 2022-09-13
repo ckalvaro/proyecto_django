873537efb8596604d28439c7c756973b9edc3cf3
ckalvaro <alvarochany@gmail.com>
2022-09-13 12:31:50 -0300

    Edición de noticias

--- Stamps/AppBlog/urls.py
+++ Stamps/AppBlog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from AppBlog.views import inicio, noticia_detalle_view, form_noticias
+from AppBlog.views import inicio, noticia_detalle_view, form_noticias, editar_noticia
 app_name = 'AppBlog'
 urlpatterns = [
     path('', inicio.as_view(), name='inicio'), 
     path('noticia/<int:pk>', noticia_detalle_view.as_view(), name='detalle'),
     path('noticia/nueva_noticia', form_noticias, name='form_noticia'),
+    path('noticia/editar/<int:pk>', editar_noticia.as_view(), name='editar_noticia'),
 ]
