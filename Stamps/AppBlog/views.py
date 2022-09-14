337dbf8f96036d7005cd93468d88e89c5d0c923c
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-09-14 08:50:07 -0300

    Creo formulario para crear usuario en views

--- Stamps/AppBlog/views.py
+++ Stamps/AppBlog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from AppBlog.models import Noticia
-from AppBlog.forms import FormularioNoticia
+from AppBlog.models import Noticia, Usuario
+from AppBlog.forms import FormularioNoticia, FormularioUsuario
 
 # Create your views here.
 def prueba(request):
@@ -27,3 +27,27 @@ def form_noticias(request):
 def noticias(request):
     noticias = Noticia.objects.all()
     return render(request, 'AppBlog/noticias.html', {'noticias':noticias})
+
+def form_usuarios(request):
+    if request.method == 'POST':
+        form = FormularioUsuario(request.POST)
+        if form.is_valid():
+            info = form.cleaned_data
+            nombre = info.get('nombre')
+            apellido = info.get('apellido')
+            email = info.get('email')
+            nombre_de_usuario = info.get('nombre_de_usuario')
+            # contraseña1 = info.get('contrseña1')
+            # contraseña2 = info.get('contrseña2')
+            usuario = Usuario (nombre = nombre, apellido = apellido, email = email, nombre_de_usuario = nombre_de_usuario)
+            usuario.save()
+            mensaje = "Carga exitosa"
+            return render(request, 'AppBlog/formulario_usuario.html', {'mensaje':mensaje})
+    else:
+        form = FormularioUsuario()
+        mensaje = "Rellene el formulario"
+        return render(request, 'AppBlog/formulario_usuario.html', {'mensaje':mensaje, 'form':form})
+
+def usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'AppBlog/usuarios.html', {'usuarios':usuarios})
