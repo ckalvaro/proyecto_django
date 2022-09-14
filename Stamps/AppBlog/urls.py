373e74cf901ce54cfc8780363c9db56d110b36db
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-09-14 08:49:38 -0300

    Path para formulario y template de usuario

--- Stamps/AppBlog/urls.py
+++ Stamps/AppBlog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from AppBlog.views import prueba, form_noticias, noticias
+from AppBlog.views import prueba, form_noticias, noticias, form_usuarios, usuarios
 
 urlpatterns = [
     path('prueba/', prueba, name='prueba'),
     path('form_noticia/', form_noticias, name='form_noticia'),
     path('noticias/', noticias, name='noticias'),
+    path('form_usuario/', form_usuarios, name= 'form_usuario'),
+    path('usuarios/', usuarios, name = 'usuarios'),
 ]
