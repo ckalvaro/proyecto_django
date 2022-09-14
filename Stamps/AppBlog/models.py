7014209045b59f657291a57992008576c956b7af
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-09-13 22:59:51 -0300

    Creación de obj usuario

--- Stamps/AppBlog/models.py
+++ Stamps/AppBlog/models.py
@@ -15,3 +15,14 @@ class Noticia(models.Model):
 
     def __str__(self):
         return self.titulo + " - " + str(self.autor_nombre)
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    nombre_de_usuario = models.CharField(max_length = 80)
+    contraseña1=models.CharField(max_length = 16, min_length = 8)
+    contraseña2=models.CharField(max_length = 16, min_length = 8)
+
+    def  __str__(self):
+        return str(self.nombre_de_usuario)
